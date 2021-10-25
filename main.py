338e08c5dbbc96b21bc8105566fdd810cd08af2d
Ish2504 <87877194+Ish2504@users.noreply.github.com>
2021-10-25 20:20:55 +0000

    done

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ while not bidding_is_finished:
     bid = int(input("What's your bid?: $"))
     bidders_list(name, bid)
   else:
-
+    clear()
+    max_bidder_value = max(bidders.values())
+    for k,v in bidders.items():
+      if v == max_bidder_value:
+        print(f"The winner is {k} with a bid of ${v}")
     bidding_is_finished = True
-print(bidders.values())
